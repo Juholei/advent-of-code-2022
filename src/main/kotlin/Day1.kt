da8b9d8eacd0a064e282db33f4854a5d09ee7503
Juho Leinonen <juholei@gmail.com>
2022-12-03 12:24:45 +0200

    Solve day 1 part 2

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,8 +1,18 @@
-fun day1(input: String): Int {
+fun day1Part1(input: String): Int {
     val splitInput = input.split("\n\n")
     val output = splitInput.map { it.split("\n") }
         .map { inventory -> inventory.map { item -> item.toInt() } }
         .map { inventory -> inventory.sum() }
     println(output.max())
     return output.max()
+}
+
+fun day1Part2(input: String): Int {
+    val splitInput = input.split("\n\n")
+    val output = splitInput.map { it.split("\n") }
+        .map { inventory -> inventory.map { item -> item.toInt() } }
+        .map { inventory -> inventory.sum() }
+        .sorted()
+        .takeLast(3)
+    return output.sum()
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -2,25 +2,31 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day1Test {
+    private val inputString =
+        """
+        1000
+        2000
+        3000
+    
+        4000
+    
+        5000
+        6000
+    
+        7000
+        8000
+        9000
+    
+        10000
+        """.trimIndent()
+
     @Test
-    fun testDay1() {
-        val inputString =
-            """
-            1000
-            2000
-            3000
-        
-            4000
-        
-            5000
-            6000
-        
-            7000
-            8000
-            9000
-        
-            10000
-            """.trimIndent()
-        assertEquals(24000, day1(inputString))
+    fun testDay1Part1() {
+        assertEquals(24000, day1Part1(inputString))
+    }
+
+    @Test
+    fun testDay1Part2() {
+        assertEquals(45000, day1Part2(inputString))
     }
 }
