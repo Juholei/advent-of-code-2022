8a95979fb2d8eccf4460f0ccca775a72e78a2ae2
Juho Leinonen <juholei@gmail.com>
2022-12-06 12:39:07 +0200

    Solve day 4 part 1

---
+++ src/main/kotlin/advent/of/code/day4/day4.kt
@@ -0,0 +1,25 @@
+package advent.of.code.day4
+
+data class ElfPair(val elf1Assignments: List<Int>, val elf2Assignments: List<Int>)
+
+fun day4Part1(input: String): Int {
+    val output = input.split("\n")
+        .map { it.split(",") }
+        .map {
+            val elf1 = it[0].split("-")
+                .map { section -> section.toInt() }
+            val elf2 = it[1].split("-")
+                .map { section -> section.toInt() }
+            ElfPair(
+                IntRange(elf1[0], elf1[1]).toList(),
+                IntRange(elf2[0], elf2[1]).toList()
+            )
+        }
+        .map { elfPair ->
+            elfPair.elf1Assignments.containsAll(elfPair.elf2Assignments) ||
+                    elfPair.elf2Assignments.containsAll(elfPair.elf1Assignments)
+        }
+        .filter { it }
+    println(output)
+    return output.count()
+}
---
+++ src/test/kotlin/Day4KtTest.kt
@@ -0,0 +1,19 @@
+import advent.of.code.day4.day4Part1
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day4KtTest {
+    private val inputString = """
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+    """.trimIndent()
+
+    @Test
+    fun day4Part1() {
+        assertEquals(2, day4Part1(inputString))
+    }
+}
